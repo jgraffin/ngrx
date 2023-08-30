f3b01fe0c4c052230571c91b896e16e557b969cf
Julio Cezar Ferreira <jcferreira1@stefanini.com>
2023-08-30 17:59:05 -0300

    Add mock data

--- src/app/components/node-dialogs/node-dialogs.component.ts
+++ src/app/components/node-dialogs/node-dialogs.component.ts
@@ -21,33 +21,6 @@ export class NodeDialogsComponent implements OnInit {
   value!: number;
 
   dialogs: any[] = [];
-  dialogsChildren = {
-    total: 4,
-    currentPage: 0,
-    pageSize: 4,
-    data: [
-      {
-        id: 'teste item filho',
-        workspaceId: 'caed00fa-3798-4879-8e98-3f363e7ab3df',
-        title: 'teste item filho titulo',
-        conditions: 'teste item filho',
-        previousSibling: 'node_99ea11cd-7901-446f-bb90-7a3575a958d2',
-        type: 'FRAME',
-        outputs: [
-          {
-            id: 'ce2d87ac-aef8-42c5-93a6-d6c95d36fdf2',
-            selectionPolicy: 'SEQUENTIAL',
-            delimiter: null,
-            values: ['Olá. Como posso te ajudar?'],
-            type: 'TEXT',
-          },
-        ],
-        slots: [],
-        created: '2023-08-16T15:45:19.649+0000',
-        updated: '2023-08-16T16:50:34.454+0000',
-      },
-    ],
-  };
 
   constructor(private store: Store) {}
 
@@ -62,8 +35,6 @@ export class NodeDialogsComponent implements OnInit {
     let currElem = event.target;
 
     const styles = {
-      attribute: 'style',
-      autoHeight: 'auto',
       children: '.dialogs-content .children',
       elementClassName: 'dialogs-content--accordion',
       folderElement: '.dialogs-content >  div',
